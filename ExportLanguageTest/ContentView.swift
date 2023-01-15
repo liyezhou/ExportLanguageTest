d2ac13c8e8b0f542a3c153c66a2ad67cef46fff8
Yezhou <liyezhou123@gmail.com>
2023-01-15 20:32:25 +0000

    Delete Mac as Destination in Target - now it doesn't export localisation

--- ExportLanguageTest/ContentView.swift
+++ ExportLanguageTest/ContentView.swift
@@ -13,7 +13,7 @@ struct ContentView: View {
             Image(systemName: "globe")
                 .imageScale(.large)
                 .foregroundColor(.accentColor)
-            Text("Hello, Test Localisation!")
+            Text("Hello, Test Localisation! Oh no it doesn't export successfully!")
         }
         .padding()
     }
