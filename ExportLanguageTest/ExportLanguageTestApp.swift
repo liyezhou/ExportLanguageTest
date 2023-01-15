a841e1e8a87a4e79e20a290f691546703a2b16d8
Yezhou <liyezhou123@gmail.com>
2023-01-15 20:29:01 +0000

    Initial Commit

---
+++ ExportLanguageTest/ExportLanguageTestApp.swift
@@ -0,0 +1,17 @@
+//
+//  ExportLanguageTestApp.swift
+//  ExportLanguageTest
+//
+//  Created by Yezhou Li on 15/01/2023.
+//
+
+import SwiftUI
+
+@main
+struct ExportLanguageTestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
